934db9f28850fce84b7d91c53ea5a68f0408d145
Obi.M <mail@obimadu.pro>
2024-06-19 00:23:12 +0100

    feat: implement internals/jwt package

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.3
 require (
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	golang.org/x/crypto v0.24.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/driver/postgres v1.5.9
---
+++ internals/jwt/jwt.go
@@ -0,0 +1,38 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/obiMadu/goJWTAuth/internals/models"
+)
+
+var JwtKey []byte
+
+type JwtClaim struct {
+	UserID int    `json:"userId" binding:"required"`
+	Email  string `json:"email" binding:"required"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(user *models.User) (string, error) {
+	// create expiration time
+	expirationTime := time.Now().Add(24 * time.Hour)
+
+	claims := JwtClaim{
+		UserID: user.ID,
+		Email:  user.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(JwtKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
