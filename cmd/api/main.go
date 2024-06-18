534bc1e644878dd6b4738b92fe6558d78e37ce3e
Obi.M <mail@obimadu.pro>
2024-06-19 00:25:46 +0100

    feat: main to initialize via config.Config()

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,15 @@
 package main
 
+import (
+	"github.com/obiMadu/goJWTAuth/internals/config"
+)
+
 const webPort string = ":8080"
 
 func main() {
+	// config
+	config.Config()
 
 	// start http server
 	routes().Run(webPort)
-
 }
