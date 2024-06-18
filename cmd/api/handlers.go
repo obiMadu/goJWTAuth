29b570ff8610a5e3d3a9e3e4deda9797b5041919
Obi.M <mail@obimadu.pro>
2024-06-18 02:04:39 +0100

    feat: create initial login handler; no jwt features yet

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,3 +1,55 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/obiMadu/goJWTAuth/internals/db"
+	"github.com/obiMadu/goJWTAuth/internals/models"
+)
+
 // TODO: create handlers
+func login(c *gin.Context) {
+
+	db := db.RawDB()
+
+	requestPayload := jsonRequest{}
+
+	err := c.ShouldBindBodyWithJSON(&requestPayload)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, jsonResponse{
+			Status:  "error",
+			Message: "Invalid credentials.",
+		})
+		return
+	}
+
+	// validate the user against the database
+	user, err := models.GetByEmail(db, requestPayload.Email)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, jsonResponse{
+			Status:  "error",
+			Message: "Invalid credentials.",
+		})
+		return
+	}
+
+	valid, err := models.PasswordMatches(user, requestPayload.Password)
+	if err != nil || !valid {
+		c.JSON(http.StatusBadRequest, jsonResponse{
+			Status:  "error",
+			Message: "Invalid credentials.",
+		})
+		return
+	}
+
+	payload := jsonResponse{
+		Status:  "success",
+		Message: "Logged In successfully.",
+		Data: gin.H{
+			"token": "jwt",
+		},
+	}
+
+	c.JSON(http.StatusOK, payload)
+}
