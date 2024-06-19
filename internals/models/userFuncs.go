c8f2675c1d0e19094b3733ce96eaf56158f99a25
Obi.M <mail@obimadu.pro>
2024-06-19 03:12:38 +0100

    feat: add get user by ID func

--- internals/models/userFuncs.go
+++ internals/models/userFuncs.go
@@ -8,6 +8,34 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// GetOne returns one user by id
+func GetOne(db *sql.DB, id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from users where id = $1`
+
+	var user User
+	row := db.QueryRowContext(ctx, query, id)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
 // GetByEmail returns one user by email
 func GetByEmail(db *sql.DB, email string) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
